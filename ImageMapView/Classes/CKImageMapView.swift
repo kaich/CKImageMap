024b552708cded3767dee8dcee8606311602e10e
mk <mk@mkdeiMac.local>
2017-12-02 17:21:39 +0800

    init

--- ImageMapView/Classes/CKImageMapView.swift
+++ ImageMapView/Classes/CKImageMapView.swift
@@ -20,6 +20,7 @@ public class CKImageMapView: UIView {
     private var mapImage: UIImage?
     private var isInitialScaled = false //处理开始frame大小为0的情况
     private let popTip = PopTip()
+    private var annotationViews = [CKMapAnotationView]()
     
     
     override init(frame: CGRect) {
@@ -37,6 +38,11 @@ public class CKImageMapView: UIView {
         scrollView.addSubview(ivMap)
         
         popTip.bubbleColor = UIColor.white
+        popTip.shouldDismissOnTap = true
+        
+        let tapGes = UITapGestureRecognizer(target: self, action: #selector(userDoubleTappedScrollview(recognizer:)))
+        tapGes.numberOfTapsRequired = 2
+        ivMap.addGestureRecognizer(tapGes)
 
     }
     
@@ -82,19 +88,52 @@ public class CKImageMapView: UIView {
     }
     
     func layoutAnnotations() {
+        annotationViews = []
         for marker in markers {
             let annotationView = CKMapAnotationView(marker: marker)
             annotationView.clickBlock = { annotationView in
                 self.showPopView(annotationView: annotationView)
             }
             ivMap.addSubview(annotationView)
+            annotationViews.append(annotationView)
             
         }
     }
     
     func showPopView(annotationView: CKMapAnotationView) {
         let infoView = CKMapPopView(marker: annotationView.marker!)
-        popTip.show(customView: infoView, direction: .up, in: ivMap, from: annotationView.frame)
+        let annoFrame = ivMap.convert(annotationView.frame, to: scrollView)
+        popTip.show(customView: infoView, direction: .up, in: scrollView, from: annoFrame)
+    }
+    
+    @objc func userDoubleTappedScrollview(recognizer:  UITapGestureRecognizer) {
+        let scrollV = scrollView
+        if (scrollV.zoomScale > scrollV.minimumZoomScale) {
+            scrollV.setZoomScale(scrollV.minimumZoomScale, animated: true)
+        }
+        else {
+            //(I divide by 3.0 since I don't wan't to zoom to the max upon the double tap)
+            let zoomRect = self.zoomRectForScale(scale: scrollV.maximumZoomScale / 3.0, center: recognizer.location(in: recognizer.view))
+            self.scrollView.zoom(to: zoomRect, animated: true)
+        }
+    }
+    
+    func zoomRectForScale(scale : CGFloat, center : CGPoint) -> CGRect {
+        var zoomRect = CGRect.zero
+        let imageV = ivMap
+        zoomRect.size.height = imageV.frame.size.height / scale;
+        zoomRect.size.width  = imageV.frame.size.width  / scale;
+        let newCenter = imageV.convert(center, from: self.scrollView)
+        zoomRect.origin.x = newCenter.x - ((zoomRect.size.width / 2.0));
+        zoomRect.origin.y = newCenter.y - ((zoomRect.size.height / 2.0));
+        
+        return zoomRect;
+    
+    }
+    
+    func rectToImageWith(scale: CGFloat, from: CGRect) -> CGRect {
+        let finalFrame = CGRect(x: from.origin.x * scale, y: from.origin.y * scale, width: from.width, height: from.height)
+        return finalFrame
     }
     
 }
@@ -112,6 +151,11 @@ extension CKImageMapView :  UIScrollViewDelegate {
         let offsetX = max((scrollView.bounds.size.width - scrollView.contentSize.width) / 2, 0)
         let offsetY = max((scrollView.bounds.size.height - scrollView.contentSize.height) / 2, 0)
         subView?.center = CGPoint(x: scrollView.contentSize.width * 0.5 + offsetX, y: scrollView.contentSize.height * 0.5 + offsetY)
+        
+        for annoView in annotationViews {
+            let finalPoint = ivMap.convert((annoView.marker?.point)!, to: scrollView)
+            annoView.center = finalPoint
+        }
     }
     
 }
